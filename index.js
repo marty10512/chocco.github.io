00f57d06b29e3edc8d58687987dc9d9e55af9b27
Andrey Martemyanov <marty105k@gmail.com>
2019-09-02 17:22:00 +0300

    рабочее меню

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const hMenuLink = document.querySelector('#burger-link');
 const hMenuStatus = document.querySelector('#burger');
-const q = hMenuStatus.classList;
+const hMenuClass = hMenuStatus.classList;
+const noScroll = document.querySelector('#scroll');
+const hMenuSecLink = document.querySelector('.fs-menu');
 
 hMenuLink.addEventListener("click", function(e) {
 
-  if (q == 'hamburger__menu') {
+  if (hMenuClass == 'hamburger__menu') {
     hMenuStatus.classList.add("hamburger__menu--active");
     hMenuStatus.classList.remove("hamburger__menu");
+    noScroll.classList.add("no-scroll");
   } else {
     hMenuStatus.classList.add("hamburger__menu");
     hMenuStatus.classList.remove("hamburger__menu--active");
+    noScroll.classList.remove("no-scroll");
   }
 
+})
+
+hMenuSecLink.addEventListener("click", function (e) {
+  hMenuStatus.classList.add("hamburger__menu");
+  hMenuStatus.classList.remove("hamburger__menu--active");
+  noScroll.classList.remove("no-scroll");
 })
