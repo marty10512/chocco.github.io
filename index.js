80947903f295b84682ff79016b9da1f167470383
Andrey Martemyanov <marty105k@gmail.com>
2019-09-10 20:25:32 +0300

    добавлен функционал инпутов

--- index.js
+++ index.js
@@ -116,3 +116,146 @@ for (commentId=0;commentId<commentNav.length;commentId++){
 
 
 ////////////////////////////////////////////// delivery form
+
+const inpName = document.querySelector('#inpName');
+const inpPhone = document.querySelector('#inpPhone');
+const inpStreet = document.querySelector('#inpStreet');
+const inpHouse = document.querySelector('#inpHouse');
+const inpBuild = document.querySelector('#inpBuild');
+const inpApart = document.querySelector('#inpApart');
+const inpFloor = document.querySelector('#inpFloor');
+const inpBtn = document.querySelector('#inpBtn');
+const inpForm = document.querySelector('.form');
+const inpComment = document.querySelector('#inpComment');
+const inpOverlay = document.querySelector('.overlay');
+const overlayBtn = document.querySelector('#overlay-btn');
+const overlayMessage = document.querySelector('.overlay__messege-text');
+
+
+inpName.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key.match(/[а-я]/i) || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace'){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+})
+
+
+
+inpPhone.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if (event.key >= 0 || event.key <= 9 || event.key == '+' || event.key == '(' || event.key == ')' || event.key == '-' ||
+  event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace') {
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+});
+
+inpStreet.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key.match(/[а-я]/i) || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace' || event.key == '-' || event.key >= 0 || event.key <= 9){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+});
+
+inpHouse.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key >= 0 || event.key <= 9 || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace'){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+});
+
+inpBuild.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key.match(/[а-я]/i) ||event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace' || event.key == '-' || event.key == '/' || event.key >= 0 || event.key <= 9){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+})
+
+inpApart.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key >= 0 || event.key <= 9 || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace'){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+});
+
+inpFloor.addEventListener('keydown', function(event){
+  let keyCheck = false;
+  if(event.key >= 0 || event.key <= 9 || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Delete' || event.key == 'Backspace'){
+    keyCheck = true;
+  }
+  if (!keyCheck ) {
+    event.preventDefault();
+  }
+});
+
+inpBtn.addEventListener('click', function(e){
+  event.preventDefault();
+  if (validateForm(inpForm)) {
+    let  data = {
+      name: inpForm.elements.name.value,
+      phone: inpForm.elements.phone.value,
+      comment: inpForm.elements.comment.value,
+      to: 'any@gmail.com'
+    }
+
+      console.log(data);
+      const xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('POST','https://webdev-api.loftschool.com/sendmail');
+      xhr.send(JSON.stringify(data)); // xhr.send(data); xhr.send(JSON.stringify(data));
+      xhr.addEventListener('load', () => {
+        if(xhr.response.status){
+          overlayMessage.textContent = 'Сообщение отправлено';
+          inpOverlay.classList.add('overlay--active').reset();
+        } else {
+          overlayMessage.textContent = 'Произошла ошибка';
+          inpOverlay.classList.add('overlay--active');
+        }
+      })
+  } else {
+    overlayMessage.textContent = 'Заполните необходимые поля';
+    inpOverlay.classList.add('overlay--active');
+  }
+})
+
+overlayBtn.addEventListener('click', function(e){
+  event.preventDefault();
+  inpOverlay.classList.remove('overlay--active');
+})
+
+function validateForm(form) {
+  let valid = true;
+  if(!validateField(form.elements.inpName)){
+    valid = false;
+  }
+  if(!validateField(form.elements.inpPhone)){
+    valid = false;
+  }
+  if(!validateField(form.elements.inpComment)){
+    valid = false;
+  }
+
+  return valid; 
+}
+
+function validateField(field) {
+    field.nextElementSibling.textContent = field.validationMessage;
+    return field.checkValidity();
+}
